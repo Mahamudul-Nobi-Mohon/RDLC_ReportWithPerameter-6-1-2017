f0e5638fa0660052be552e871184bcffdd698357
mahamudul-nobi-mohon <mn.mohon@gmail.com>
2017-01-07 00:15:40 +0600

    new file

---
+++ RDLC_ReportWithParameters_1-6-2017/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace RDLC_ReportWithParameters_1_6_2017
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtFromDate = new System.Windows.Forms.DateTimePicker();
+            this.showButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtToDate = new System.Windows.Forms.DateTimePicker();
+            this.reportViewer = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.GetOrdersReport_ResultBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.GetOrdersReport_ResultBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Date:";
+            // 
+            // dtFromDate
+            // 
+            this.dtFromDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtFromDate.Location = new System.Drawing.Point(121, 29);
+            this.dtFromDate.Name = "dtFromDate";
+            this.dtFromDate.Size = new System.Drawing.Size(114, 20);
+            this.dtFromDate.TabIndex = 1;
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(471, 30);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(75, 23);
+            this.showButton.TabIndex = 2;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "To Date:";
+            // 
+            // dtToDate
+            // 
+            this.dtToDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtToDate.Location = new System.Drawing.Point(330, 29);
+            this.dtToDate.Name = "dtToDate";
+            this.dtToDate.Size = new System.Drawing.Size(114, 20);
+            this.dtToDate.TabIndex = 1;
+            // 
+            // reportViewer
+            // 
+            reportDataSource1.Name = "Orders";
+            reportDataSource1.Value = this.GetOrdersReport_ResultBindingSource;
+            this.reportViewer.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer.LocalReport.ReportEmbeddedResource = "RDLC_ReportWithParameters_1_6_2017.OrdersReport.rdlc";
+            this.reportViewer.Location = new System.Drawing.Point(12, 74);
+            this.reportViewer.Name = "reportViewer";
+            this.reportViewer.Size = new System.Drawing.Size(763, 345);
+            this.reportViewer.TabIndex = 3;
+            // 
+            // GetOrdersReport_ResultBindingSource
+            // 
+            this.GetOrdersReport_ResultBindingSource.DataSource = typeof(RDLC_ReportWithParameters_1_6_2017.GetOrdersReport_Result);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 431);
+            this.Controls.Add(this.reportViewer);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.dtToDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtFromDate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.GetOrdersReport_ResultBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtFromDate;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtToDate;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer;
+        private System.Windows.Forms.BindingSource GetOrdersReport_ResultBindingSource;
+    }
+}
+
---
+++ RDLC_ReportWithParameters_1-6-2017/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RDLC_ReportWithParameters_1_6_2017
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer.RefreshReport();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            //string f = dtFromDate.Value.Date.ToShortDateString
+           using( NorthwindEntities db = new NorthwindEntities()){
+               //GetOrdersReport_ResultBindingSource
+               GetOrdersReport_ResultBindingSource.DataSource = db.GetOrdersReport(dtFromDate.Value, dtToDate.Value).ToList();
+               Microsoft.Reporting.WinForms.ReportParameter[] rParams = new Microsoft.Reporting.WinForms.ReportParameter[]{
+                   new Microsoft.Reporting.WinForms.ReportParameter("fromDate",dtFromDate.Value.Date.ToShortDateString()),
+                   new Microsoft.Reporting.WinForms.ReportParameter("toDate",    dtToDate.Value.Date.ToShortDateString())
+               };
+               reportViewer.LocalReport.SetParameters(rParams);
+               reportViewer.RefreshReport();
+            }
+        }
+    }
+}
